51026a269f711f673923b0953ba9877a2cb57908
randomName1235435 <77967996+randomName1235435@users.noreply.github.com>
2021-07-26 17:22:31 +0200

    initial commit

---
+++ SimpleDonutChart/SimpleDonutChart/DonutChart/PercentageConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace SimpleDonutChart.DonutChart
+{
+    public class PercentageConverter : IValueConverter
+    {
+        public object Convert(object value,
+            Type targetType,
+            object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            double outParam;
+            if (double.TryParse(value.ToString(), out outParam))
+            {
+                if (outParam > 100) return (double)0;
+                return (360) - (outParam * System.Convert.ToDouble(3.6));
+            }
+            return (double)0;
+        }
+        public object ConvertBack(object value,
+            Type targetType,
+            object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            return 0.0;
+        }
+    }
+}
